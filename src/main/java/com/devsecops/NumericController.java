bd83b7651ee364f1f57b93953dbbcc2507710a86
CHIKHAOUI-Firas99 <78984442+CHIKHAOUI-Firas99@users.noreply.github.com>
2022-09-24 02:22:14 +0100

    v17

--- src/main/java/com/devsecops/NumericController.java
+++ src/main/java/com/devsecops/NumericController.java
@@ -35,7 +35,7 @@ public class NumericController {
 
 		@GetMapping("/")
 		public String welcome() {
-			return "Kubernetes DevSecOps";
+			return "Tora7 lidouuu be sfehaaa";
 		}
 
 		@GetMapping("/compare/{value}")
